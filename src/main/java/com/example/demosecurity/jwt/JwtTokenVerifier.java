3fea37ee71b467613481f949733b080590e5576f
Yuriy <62200771+MusienkoYuriy97@users.noreply.github.com>
2021-06-24 21:27:19 +0300

    Update JwtTokenVerifier.java

--- src/main/java/com/example/demosecurity/jwt/JwtTokenVerifier.java
+++ src/main/java/com/example/demosecurity/jwt/JwtTokenVerifier.java
@@ -75,5 +75,7 @@ public class JwtTokenVerifier extends OncePerRequestFilter {
         }catch (JwtException e){
             throw new IllegalStateException(String.format("Token %s cannot be trusted", token));
         }
+        filterChain.doFilter(request,response);
+
     }
 }
